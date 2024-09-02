2dd4d37fbfda57d821d8711d93b103c5cfa7c201
bfmonster <bf.monster@proton.me>
2024-09-02 17:03:34 +0200

    initial commit

---
+++ src/HighLow.sol
@@ -0,0 +1,247 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "chainlink/vrf/dev/VRFCoordinatorV2_5.sol";
+import "chainlink/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import "openzeppelin/access/AccessControl.sol";
+import "openzeppelin/security/ReentrancyGuard.sol";
+import "openzeppelin/token/ERC20/IERC20.sol";
+import "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import "./shared/games/GameInterface.sol";
+import "./shared/CoreInterface.sol";
+import "./shared/staking/StakingInterface.sol";
+
+import "./DiceBet.sol";
+
+/**
+ * Errors used in this contract
+ *
+ * D01 - invalid staking contract
+ * D02 - player address mismatch
+ * D03 - amount invalid
+ * D04 - threshold invalid
+ * D05 - only core can place bets
+ * D06 - invalid coordinator            //no needed
+ * D07 - staking pool balance insufficient
+ */
+
+contract Dice is
+    VRFConsumerBaseV2Plus,
+    AccessControl,
+    GameInterface,
+    ReentrancyGuard
+{
+    using SafeERC20 for IERC20;
+
+    bytes32 public constant TIMELOCK = keccak256("TIMELOCK");
+
+    uint256 public constant REQUIRED_FUNDS_COEFFICIENT = 20;
+    uint256 private immutable created;
+    uint256 private immutable subscriptionId;
+    address public immutable vrfCoordinator;
+    bytes32 public immutable keyHash;
+    uint32 private constant callbackGasLimit = 2_500_000;
+    uint16 public constant requestConfirmations = 3;
+    uint32 private constant numWords = 1;
+
+    StakingInterface public staking;
+    CoreInterface public core;
+
+    mapping(address => uint256[]) public playerRequests;
+    mapping(uint256 => DiceBet) public requestBets;
+
+    mapping(uint256 => uint256) public reservedFunds;
+
+    event Rolled(
+        address indexed bet,
+        uint256 indexed requestId,
+        address roller
+    );
+    event Landed(
+        address indexed bet,
+        uint256 indexed requestId,
+        address roller,
+        uint256 indexed result
+    );
+
+    constructor(
+        address _core,
+        address _staking,
+        address _admin,
+        uint256 _subscriptionId,
+        address _vrfCoordinator,
+        bytes32 _keyHash
+    ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
+        require(_vrfCoordinator != address(0), "D06");
+        vrfCoordinator = _vrfCoordinator;
+        keyHash = _keyHash;
+        subscriptionId = _subscriptionId;
+        core = CoreInterface(_core);
+        require(core.isStaking(_staking), "D01");
+        staking = StakingInterface(_staking);
+        created = block.timestamp;
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+    }
+
+    /* For Test Purpose - will be removed when deployment */
+    // Public function to test fulfillRandomWords
+    function FulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) public {
+        fulfillRandomWords(requestId, randomWords);
+    }
+
+    function getVrfCoordinator() public view returns (address) {
+        return vrfCoordinator;
+    }
+    function getSubscriptionId() public view returns (uint256) {
+        return subscriptionId;
+    }
+    function getKeyHash() public view returns (bytes32) {
+        return keyHash;
+    }
+
+    /* Test Purpose Functions definition Finish */
+
+    // Get possible win amount. In Dice game, this will be same as the win amount.
+    // This will return the winner's prize amount according to _threshold and _side
+    // _side = 1 -> win when the random number is bigger than _threshold
+    // _side = 0 -> win when the random number is less than _threshold
+    // if win percent is higher, the profit will be smaller
+    // so when _threshold is 5000, the profit will be maximaized - 2Xamount
+    function getPossibleWin(
+        uint256 _threshold,
+        bool _side,
+        uint256 _amount
+    ) public pure returns (uint256) {
+        require(_threshold > 0 && _threshold < 10000, "D04");
+
+        // Calculate mulplier according to the success percentage
+        if (!_side) {
+            return (10000 * _amount) / _threshold;
+        } else {
+            return (10000 * _amount) / (10000 - _threshold);
+        }
+    }
+
+    // Will start the game
+    function roll(
+        address player,
+        uint256 amount,
+        uint256 threshold,
+        bool side
+    ) internal nonReentrant returns (address) {
+        // validate threshold
+        require(threshold < 10000 && threshold > 0, "D04");
+        // request random number
+        uint256 requestId = VRFCoordinatorV2_5(vrfCoordinator)
+            .requestRandomWords(
+                VRFV2PlusClient.RandomWordsRequest({
+                    keyHash: keyHash,
+                    subId: subscriptionId,
+                    requestConfirmations: requestConfirmations,
+                    callbackGasLimit: callbackGasLimit,
+                    numWords: numWords,
+                    extraArgs: VRFV2PlusClient._argsToBytes(
+                        VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                    )
+                })
+            );
+        // create and register bet
+        DiceBet bet = new DiceBet(player, amount, 1, address(this));
+        // bet.setRequestId(requestId);
+        bet.setBets(threshold, side);
+        requestBets[requestId] = bet;
+        //transfer betted money into staking
+        IERC20(staking.getToken()).transfer(address(staking), amount);
+        playerRequests[player].push(requestId);
+        emit Rolled(address(bet), requestId, player);
+        // calculate max possible win
+        uint256 possibleWin = getPossibleWin(threshold, side, amount);
+        uint256 stakingBalance = IERC20(staking.getToken()).balanceOf(
+            address(staking)
+        );
+        require(stakingBalance >= possibleWin, "D07");
+        // transfer required amount to dice balance
+        staking.reserveFunds(possibleWin);
+        // reserve funds
+        reservedFunds[requestId] = possibleWin;
+        return address(bet);
+    }
+
+    function placeBet(
+        address _player,
+        uint256 _amount,
+        bytes calldata _data
+    ) external override returns (address betAddress) {
+        require(address(core) == _msgSender(), "D05");
+        (address player, uint256 amount, uint256 _threshold, bool _side) = abi
+            .decode(_data, (address, uint256, uint256, bool));
+        //revert if player is not the same
+        require(player == _player, "D02");
+        //revert if amount is not whole
+        require(amount * 10 ** 18 == _amount, "D03");
+
+        return address(roll(_player, _amount, _threshold, _side));
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {
+        uint256 random = randomWords[0];
+        //get random number between 0 and 10000
+        uint256 value = (random % 9999) + 1;
+        DiceBet bet = requestBets[requestId];
+        address player = bet.getPlayer();
+        (uint256 threshold, bool side) = bet.getBets();
+        bet.setWinNumber(value);
+        bet.setStatus(2);
+
+        // Calculate profit according to the result.
+        uint256 amount = 0;
+        if ((value > threshold) == side) {
+            amount = getPossibleWin(threshold, side, bet.getAmount());
+        }
+        bet.setResult(amount);
+        if (amount > 0) {
+            // send win amount to player
+            IERC20(core.token()).transfer(player, amount);
+        }
+        // send leftovers back to staking contract - this will be 0 in dice game
+        IERC20(core.token()).transfer(
+            address(staking),
+            reservedFunds[requestId] - amount
+        );
+        // clear reserved funds for current request
+        reservedFunds[requestId] = 0;
+        emit Landed(address(bet), requestId, player, value);
+    }
+
+    function getPlayerRequestsCount(
+        address player
+    ) public view returns (uint256) {
+        return playerRequests[player].length;
+    }
+
+    function getRequestBet(uint256 requestId) public view returns (address) {
+        return address(requestBets[requestId]);
+    }
+
+    function getAddress() public view override returns (address) {
+        return address(this);
+    }
+
+    function getVersion() public view override returns (uint256) {
+        return created;
+    }
+
+    function getFeeType() public pure override returns (uint256) {
+        return 1;
+    }
+
+    function getStaking() public view override returns (address) {
+        return address(staking);
+    }
+}
---
+++ test/HighLow.t.sol
@@ -0,0 +1,290 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/console.sol";
+import "forge-std/Test.sol";
+import "../src/shared/Token.sol";
+import "../src/shared/Core.sol";
+import "../src/shared/staking/StakingInterface.sol";
+import "../src/Dice.sol";
+import "../src/DiceBet.sol";
+
+contract DiceTest is Test {
+    Token public token;
+    address public staking = address(999000999000);
+    Core public core;
+    Dice public dice;
+    Partner public partner;
+    BetsMemory public betsMemory;
+    Pass public pass;
+    address public affiliate = address(128911982379182361);
+
+    address public alice = address(1);
+    address public bob = address(2);
+    address public carol = address(3);
+    address public dave = address(4);
+    address public eve = address(5);
+
+    function setUp() public {
+        pass = new Pass(address(this));
+        pass.grantRole(pass.TIMELOCK(), address(this));
+        pass.setAffiliate(affiliate);
+        vm.mockCall(
+            affiliate,
+            abi.encodeWithSelector(
+                AffiliateInterface.checkInviteCondition.selector,
+                address(1)
+            ),
+            abi.encode(true)
+        );
+        vm.mockCall(
+            address(pass),
+            abi.encodeWithSelector(AffiliateMember.getInviter.selector, alice),
+            abi.encode(address(0))
+        );
+        pass.mint(alice, address(0), address(0));
+
+        token = new Token(address(this));
+        betsMemory = new BetsMemory(address(this));
+        betsMemory.grantRole(betsMemory.TIMELOCK(), address(this));
+        betsMemory.setPass(address(pass));
+        core = new Core(
+            address(token),
+            address(betsMemory),
+            address(pass),
+            address(this)
+        );
+        core.grantRole(core.TIMELOCK(), address(this));
+        vm.mockCall(
+            address(staking),
+            abi.encodeWithSelector(StakingInterface.getAddress.selector),
+            abi.encode(address(staking))
+        );
+        vm.mockCall(
+            address(staking),
+            abi.encodeWithSelector(StakingInterface.getToken.selector),
+            abi.encode(address(token))
+        );
+        core.addStaking(address(staking));
+        dice = new Dice(
+            address(core),
+            address(staking),
+            address(this),
+            555,
+            0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed,
+            0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f
+        );
+        core.addGame(address(dice));
+        betsMemory.addAggregator(address(core));
+        dice.grantRole(dice.TIMELOCK(), address(this));
+        address tariff = core.addTariff(0, 1_00, 0);
+        vm.startPrank(carol);
+        partner = Partner(core.addPartner(tariff));
+        vm.stopPrank();
+        for (uint160 i = 1; i <= 100; i++) {
+            if (i > 1) {
+                pass.mint(address(i), alice, alice);
+            }
+            token.transfer(address(i), 1000 ether);
+        }
+        token.transfer(address(staking), 5000 ether);
+        token.transfer(address(core), 5000 ether);
+    }
+
+    function getRequest(uint256 requestId) internal {
+        vm.mockCall(
+            0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed,
+            abi.encodeWithSelector(
+                VRFCoordinatorV2_5.requestRandomWords.selector,
+                VRFV2PlusClient.RandomWordsRequest({
+                    keyHash: bytes32(
+                        0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f
+                    ),
+                    subId: uint256(555),
+                    requestConfirmations: uint16(3),
+                    callbackGasLimit: uint32(2_500_000),
+                    numWords: uint32(1),
+                    extraArgs: VRFV2PlusClient._argsToBytes(
+                        VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                    )
+                })
+            ),
+            abi.encode(requestId)
+        );
+    }
+
+    function testConstructor() public {
+        Dice _dice = new Dice(
+            address(core),
+            address(staking),
+            address(this),
+            555,
+            0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed,
+            0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f
+        );
+        assertEq(_dice.getAddress(), address(_dice));
+        assertEq(_dice.getStaking(), address(staking));
+        assertEq(
+            _dice.getVrfCoordinator(),
+            0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed
+        );
+        assertEq(
+            _dice.getKeyHash(),
+            0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f
+        );
+        assertEq(_dice.getSubscriptionId(), 555);
+    }
+
+    function testBrokenBet() public {
+        vm.startPrank(alice);
+        token.approve(address(core), 1000 ether);
+
+        vm.expectRevert(bytes("D02"));
+        partner.placeBet(
+            address(dice),
+            100 ether,
+            abi.encode(bob, 100, 5000, 1)
+        );
+
+        vm.expectRevert(bytes("D03"));
+        partner.placeBet(
+            address(dice),
+            100 ether,
+            abi.encode(alice, 100 ether, 5000, 1)
+        );
+
+        vm.expectRevert(bytes("D04"));
+        partner.placeBet(
+            address(dice),
+            100 ether,
+            abi.encode(alice, 100, 0, 1)
+        );
+
+        vm.expectRevert(bytes("D04"));
+        dice.getPossibleWin(0, true, 1000);
+
+        assertEq(dice.getPossibleWin(5000, true, 1000), 2000);
+        assertEq(dice.getPossibleWin(5000, false, 1000), 2000);
+
+        vm.stopPrank();
+        assertEq(token.balanceOf(address(dice)), 0);
+    }
+
+    function testFuzz_fulFillRound(uint256 _threshold) public {
+        vm.assume(_threshold > 0 && _threshold < 10000);
+        // uint256 amount = 100;
+        getRequest(5);
+        token.transfer(address(dice), 100 * 10000 ether);
+        token.transfer(address(staking), 100 * 10000 ether);
+        token.transfer(address(core), 100 * 10000 ether);
+
+        vm.startPrank(alice);
+        token.approve(address(core), 100 * 1 ether);
+
+        uint256 playBalance = token.balanceOf(alice);
+
+        address bet = partner.placeBet(
+            address(dice),
+            100 * 1 ether,
+            abi.encode(alice, 100, _threshold, 1)
+        );
+
+        DiceBet diceBet = DiceBet(bet);
+        uint256[] memory words = new uint256[](3);
+        uint256 profit = dice.getPossibleWin(_threshold, true, 100 * 1 ether);
+        uint256 stakingBalance = token.balanceOf(staking);
+
+        words[0] = 256;
+        dice.FulfillRandomWords(5, words);
+
+        assertEq(diceBet.getResult(), 257 > _threshold ? profit : 0 ether);
+        assertEq(diceBet.getStatus(), 2);
+        assertEq(diceBet.getWinNumber(), words[0] + 1);
+        assertEq(dice.getPlayerRequestsCount(alice), 1);
+        assertEq(dice.getRequestBet(5), bet);
+
+        (
+            address __player,
+            address __game,
+            uint256 __amount,
+            uint256 __result,
+            uint256 __status,
+            uint256 __created
+        ) = diceBet.getBetInfo();
+        assertEq(__player, alice);
+        assertEq(__game, address(dice));
+        assertEq(__amount, 100 * 1 ether);
+        assertEq(__result, 257 > _threshold ? profit : 0 ether);
+        assertEq(__status, 2);
+        assertEq(__created, block.timestamp);
+
+        assertEq(token.balanceOf(address(dice)), 100 * 10000 ether - profit);
+        assertEq(token.balanceOf(alice), playBalance - __amount + __result);
+        assertEq(
+            token.balanceOf(staking),
+            stakingBalance + (__result > 0 ? 0 : profit)
+        );
+
+        vm.stopPrank();
+    }
+
+    function testOnlyCore_placeBet() public {
+        // warp to 26/03/2024 11:00:00
+        vm.warp(1711450800);
+        bytes memory data = abi.encode(alice, 500, 5000, true);
+        vm.expectRevert(bytes("D05"));
+        dice.placeBet(alice, 500 ether, data);
+    }
+
+    function testInsufficientStakingFunds() public {
+        // Test scenario where staking funds are insufficient to cover the bet
+        vm.startPrank(alice);
+
+        // Mock insufficient funds in staking
+        vm.mockCall(
+            address(token),
+            abi.encodeWithSelector(IERC20.balanceOf.selector, address(staking)),
+            abi.encode(0)
+        );
+
+        token.approve(address(core), 1000 ether);
+        getRequest(100);
+
+        vm.expectRevert(bytes("D07"));
+        partner.placeBet(
+            address(dice),
+            1000 ether,
+            abi.encode(alice, 1000, 5000, 1)
+        );
+        vm.stopPrank();
+    }
+
+    function testPlaceBet() public {
+        // warp to 26/03/2024 11:00:00
+        vm.warp(1711450800);
+        //Test if the bet is placed correctly
+        vm.startPrank(alice);
+        token.approve(address(core), 1000 ether);
+        getRequest(100);
+
+        address bet = partner.placeBet(
+            address(dice),
+            1000 ether,
+            abi.encode(alice, 1000, 5000, 1)
+        );
+
+        DiceBet diceBet = DiceBet(bet);
+
+        assertEq(diceBet.getPlayer(), alice);
+        assertEq(diceBet.getAmount(), 1000 ether);
+        assertEq(diceBet.getGame(), address(dice));
+
+        (uint256 threshold, bool side) = diceBet.getBets();
+        assertEq(threshold, 5000);
+        assertEq(side, true);
+
+        assertEq(diceBet.getCreated(), block.timestamp);
+
+        vm.stopPrank();
+    }
+}
